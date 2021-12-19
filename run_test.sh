d08ac9cb6e3f63e007eac76faa677afd43e2d577
gamorosino <gamorosino@gmail.com>
2021-12-19 21:26:50 +0100

    minor changes

--- run_test.sh
+++ run_test.sh
@@ -8,7 +8,6 @@ mkdir -p ${outputdir}
 tag_list=( ACC PFM MCDs HD )
 csv_all=${outputdir}'/average_dice_score.csv'
 echo 'Category, CSF, GM, WM, DGM, Brainstem, Cerebellum' > ${csv_all}
-printf '' > ${csv_all}
 for tag in ${tag_list[@]}; do
 	bash ${SCRIPT_DIR}/predict_testset.sh ${download_dir}'/'${tag}'/'proj-${project_id} ${outputdir}'/'${tag}
 	echo ${tag},$( cat ${outputdir}'/'${tag}'/dice_score_average.csv' ) >> ${csv_all}
